09f97b10695a82e26765bc30d882d0038d374d43
Sidharth Jain <sidharthjainp9@gmail.com>
2024-09-26 10:36:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# print('Enter a todo:')
+user_prompt = "Enter a todo: "
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos= [todo1, todo2, todo3, 'Helloclea']
+print(todos)
